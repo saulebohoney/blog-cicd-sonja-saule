f6163651817bd43dc010601bdec459637d7ec8bb
Sonja Duric <duric.sonja@gmail.com>
2017-05-26 15:48:22 -0400

    travis file updated

--- test/test-blog.js
+++ test/test-blog.js
@@ -16,35 +16,35 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Blog', function() {
-    before(function() {
-        return runServer();
-    });
+  before(function() {
+    return runServer();
+  });
 
-    after(function() {
-        return closeServer();
-    });
+  after(function() {
+    return closeServer();
+  });
 
-    it('should list Blog posts on GET', function() {
-        return chai.request(app)
+  it('should list Blog posts on GET', function() {
+    return chai.request(app)
       .get('/blog-posts')
       .then(function(res) {
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.should.be.a('array');
-          res.body.length.should.be.at.least(1);
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.at.least(1);
         // each item should be an object with key/value pairs
         // for `id`, `name` and `checked`.
-          const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
-          res.body.forEach(function(item) {
-              item.should.be.a('object');
-              item.should.include.keys(expectedKeys);
-          });
+        const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
       });
-    });
+  });
 
-    it('should add an blog-post on POST', function() {
-        const newBlog = {title: 'cloudy today', author:'saule', content:'cloudy today and should be nicer tomorrow'};
-        return chai.request(app)
+  it('should add an blog-post on POST', function() {
+    const newBlog = {title: 'cloudy today', author:'saule', content:'cloudy today and should be nicer tomorrow'};
+    return chai.request(app)
       .post('/blog-posts')
       .send(newBlog)
       // .then(function(res) {
@@ -53,16 +53,52 @@ describe('Blog', function() {
       //     return chai.request(app);
       // })
       .then(function(res){
-          res.should.have.status(201);
-          res.should.be.json;
-          res.body.should.be.a('object');
-          res.body.should.include.keys('id', 'title', 'content', 'author','publishDate');
-          res.body.id.should.not.be.null;
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'title', 'content', 'author','publishDate');
+        res.body.id.should.not.be.null;
         // response should be deep equal to `newItem` from above if we assign
         // `id` to it from `res.body.id`
-          res.body.should.deep.equal(Object.assign(newBlog, {id: res.body.id},{publishDate:res.body.publishDate}));
+        res.body.should.deep.equal(Object.assign(newBlog, {id: res.body.id},{publishDate:res.body.publishDate}));
       });
-    });
+  });
+
+it('should update blog-post on PUT', function() {
+    // we initialize our updateData here and then after the initial
+    // request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updateData = {
+      title: 'cleared up',
+      author: 'Sonja',
+      content: 'was cloudy earlier but now is bright and sunny'
+    };
+
+    return chai.request(app)
+      // first have to get so we have an idea of object to update
+      .get('/blog-posts')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        updateData.publishDate = res.body[0].publishDate;
+        // this will return a promise whose value will be the response
+        // object, which we can inspect in the next `then` back. Note
+        // that we could have used a nested callback here instead of
+        // returning a promise and chaining with `then`, but we find
+        // this approach cleaner and easier to read and reason about.
+        return chai.request(app)
+          .put(`/blog-posts/${updateData.id}`)
+          .send(updateData);
+      })
+      // prove that the PUT request has right status code
+      // and returns updated item
+      .then(function(res) {
+        res.should.have.status(204);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.deep.equal(updateData);
+      });
+  });
+  
 
 
 
