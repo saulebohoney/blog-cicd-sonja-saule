005ac223cd76f9fd93022d57f538fab7669e250f
Saule BN <beyonce@thinkful.com>
2017-05-26 16:13:59 -0400

    all tests passing

--- test/test-blog.js
+++ test/test-blog.js
@@ -98,8 +98,18 @@ it('should update blog-post on PUT', function() {
         res.body.should.deep.equal(updateData);
       });
   });
-  
 
-
-
+    it('should delete blogs on DELETE', function() {
+        return chai.request(app)
+      // first have to get so we have an `id` of item
+      // to delete
+      .get('/blog-posts')
+      .then(function(res) {
+          return chai.request(app)
+          .delete(`/blog-posts/${res.body[0].id}`);
+      })
+      .then(function(res) {
+          res.should.have.status(204);
+      });
+    });
 });
