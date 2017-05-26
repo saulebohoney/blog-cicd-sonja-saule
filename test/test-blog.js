1fcf79d3f136ab82fd5881f850990fc420d9e4ac
Sonja Duric <duric.sonja@gmail.com>
2017-05-26 14:44:47 -0400

    first test created but failing

---
+++ test/test-blog.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+// this lets us use *should* style syntax in our tests
+// so we can do things like `(1 + 1).should.equal(2);`
+// http://chaijs.com/api/bdd/
+const should = chai.should();
+
+// This let's us make HTTP requests
+// in our tests.
+// see: https://github.com/chaijs/chai-http
+chai.use(chaiHttp);
+
+describe('Blog', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list Blog posts on GET', function() {
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.at.least(1);
+        // each item should be an object with key/value pairs
+        // for `id`, `name` and `checked`.
+        const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+
+});
